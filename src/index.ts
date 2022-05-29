729a03b6f5230078e9fb8ffbd084d226bf306a38
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-05-29 19:56:14 +0200

    setup ts eslint prettier config

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+var favoriteAnimal = 'cat'
+favoriteAnimal = 'dog'
+
+var nameX = 'joe'
+
+const user = {
+    name: 'john doe',
+    age: 20,
+}
+
+console.log('hello world!')
+
+const test = 'test'
+
+var x = 2
+var y = 3
