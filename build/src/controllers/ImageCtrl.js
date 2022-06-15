0286f595d4261c184bafd12c1414cee747d70c2b
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-06-15 21:46:10 +0200

    fixed query param types in controller

--- build/src/controllers/ImageCtrl.js
+++ build/src/controllers/ImageCtrl.js
@@ -1,25 +1,80 @@
 "use strict";
 // CONTROLLER => parses the request and calls the service action
 //            => error HANDLING
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ImageCtrl = void 0;
 var imageService_1 = __importDefault(require("../services/imageService"));
+var root = 'C:\\Users\\admin\\OneDrive\\Desktop\\Privat\\coding\\Masterschool\\Udacity\\BACKEND\\image processing api project\\image-processing-api-project\\images\\thumb';
 var ImageCtrl = /** @class */ (function () {
     function ImageCtrl() {
     }
     ImageCtrl.prototype.showImage = function (req, res) {
-        console.log(req.query);
-        var _a = req.query, filename = _a.filename, width = _a.width, height = _a.height;
-        var image = imageService_1.default.showImage(filename, width, height);
-        console.log(image);
-        // handle errors
-        if (!image) {
-            res.status(400).send('image not found');
-        }
-        // res.sendFile(image)
+        return __awaiter(this, void 0, void 0, function () {
+            var filename, width, height, image;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        filename = req.query.filename;
+                        width = req.query.width;
+                        height = req.query.height;
+                        if (!(!filename || !width || !height)) return [3 /*break*/, 1];
+                        res.send('query string missing - please use the following url format: /api/images?filename:YOURFILE&width:YOURWIDTH&height:YOURHEIGHT');
+                        return [3 /*break*/, 3];
+                    case 1: return [4 /*yield*/, imageService_1.default.showImage(filename, width, height)
+                        // eslint-disable-next-line no-console
+                    ];
+                    case 2:
+                        image = _a.sent();
+                        // eslint-disable-next-line no-console
+                        console.log('image', image);
+                        if (!image) {
+                            res.status(400).send('image not found');
+                        }
+                        res.sendFile("".concat(filename, ".jpg"), { root: root });
+                        _a.label = 3;
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
     };
     return ImageCtrl;
 }());
--- src/controllers/ImageCtrl.ts
+++ src/controllers/ImageCtrl.ts
@@ -2,31 +2,31 @@
 //            => error HANDLING
 
 import { Request, Response } from 'express'
-import { dirname } from 'path'
 import ImageService from '../services/imageService'
 
+const root =
+    'C:\\Users\\admin\\OneDrive\\Desktop\\Privat\\coding\\Masterschool\\Udacity\\BACKEND\\image processing api project\\image-processing-api-project\\images\\thumb'
 export class ImageCtrl {
     async showImage(req: Request, res: Response) {
-        console.log('req.query', req.query)
-        const { filename, width, height } = req.query
+        const filename = req.query.filename as unknown as string
+        const width = req.query.width as unknown as string
+        const height = req.query.height as unknown as string
 
         if (!filename || !width || !height) {
             res.send(
                 'query string missing - please use the following url format: /api/images?filename:YOURFILE&width:YOURWIDTH&height:YOURHEIGHT'
             )
+        } else {
+            const image = await ImageService.showImage(filename, width, height)
+            // eslint-disable-next-line no-console
+            console.log('image', image)
+
+            if (!image) {
+                res.status(400).send('image not found')
+            }
+
+            res.sendFile(`${filename}.jpg`, { root: root })
         }
-
-        const image = await ImageService.showImage(filename as any, width as any, height as any)
-        console.log('image', image)
-        // handle errors
-        if (!image) {
-            res.status(400).send('image not found')
-        }
-
-        // eslint-disable-next-line no-undef
-        const root = `C:\\Users\\admin\\OneDrive\\Desktop\\Privat\\coding\\Masterschool\\Udacity\\BACKEND\\image processing api project\\image-processing-api-project\\images\\thumb`
-
-        res.sendFile(`${filename}.jpg`, { root: root })
     }
 }
 
