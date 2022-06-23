7010ca5b3631128e38fa7a7cafa560e43c05f3f9
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-06-23 13:38:51 +0200

    added cache functionality in controller

--- src/controllers/ImageCtrl.ts
+++ src/controllers/ImageCtrl.ts
@@ -3,26 +3,31 @@
 
 import { Request, Response } from 'express'
 import ImageService from '../services/imageService'
+import fs from 'fs'
 
 const root =
     'C:\\Users\\admin\\OneDrive\\Desktop\\Privat\\coding\\Masterschool\\Udacity\\BACKEND\\image processing api project\\image-processing-api-project\\images\\thumb'
+
 export class ImageCtrl {
     async showImage(req: Request, res: Response): Promise<void> {
         const filename = req.query.filename as unknown as string
         const width = req.query.width as unknown as string
         const height = req.query.height as unknown as string
 
+        const path = `images/thumb/${filename}-${width}-${height}.jpg`
+
         if (!filename || !width || !height) {
             res.send(
                 'query string missing - please use the following url format: /api/images?filename:YOURFILE&width:YOURWIDTH&height:YOURHEIGHT'
             )
         } else {
-            const image = await ImageService.showImage(filename, width, height)
-            // eslint-disable-next-line no-console
-            console.log('image', image)
+            if (!fs.existsSync(path)) {
+                console.log('creating new image')
 
-            if (!image) {
-                res.status(400).send('image not found')
+                const image = await ImageService.showImage(filename, width, height)
+                if (!image) {
+                    res.status(400).send('image not found')
+                }
             }
 
             res.sendFile(`${filename}-${width}-${height}.jpg`, { root: root })
