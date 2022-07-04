d9cad6799a27a6adec1b38d0fc0e7203ea7b1a7a
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-07-04 19:53:52 +0200

    tried to fix jest open handle warning - added afterEach server.close

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,33 +1,40 @@
 import app from '../src/app'
-import supertest from 'supertest'
+import request from 'supertest'
+import { Server } from 'http'
 
-const agent = supertest(app)
+let server: Server
 
-describe('image controller', (): void => {
-    test('serving correct query parameters should retun status code 200', async (): Promise<void> => {
-        const response = await agent.get('/api/images?filename=sahara-desert&width=300&height=300')
-        expect(response.status).toBe(200)
+describe('image controller', () => {
+    beforeEach(() => {
+        server = app.listen()
+    })
+    afterEach(async () => {
+        // eslint-disable-next-line no-console
+        await server.close(() => console.log('server closing'))
     })
 
-    test('a missing query parameter should return status code 400', async (): Promise<void> => {
-        const response = await agent.get('/api/images?filename=sahara-desert&width=300')
-        expect(response.status).toBe(400)
-    })
-
-    test('a missing query parameter should return a message for the user', async (): Promise<void> => {
-        const response = await agent.get('/api/images?filename=sahara-desert&width=300')
-        expect(response.text).toBe(
-            'query string missing - please use the following url format: /api/images?filename:YOURFILE&width:YOURWIDTH&height:YOURHEIGHT'
-        )
-    })
-
-    test('querying a non-existing filename should return status code 400', async (): Promise<void> => {
-        const response = await agent.get('/api/images?filename=non-existent&width=300&height=300')
-        expect(response.status).toBe(400)
-    })
-
-    test('querying a non-existing filename should return a message for the user', async (): Promise<void> => {
-        const response = await agent.get('/api/images?filename=non-existent&width=300&height=300')
-        expect(response.text).toBe('image not found')
+    describe('image controller', (): void => {
+        test('serving correct query parameters should retun status code 200', async (): Promise<void> => {
+            const response = await request(server).get('/api/images?filename=sahara-desert&width=300&height=300')
+            expect(response.status).toBe(200)
+        })
+        test('a missing query parameter should return status code 400', async (): Promise<void> => {
+            const response = await request(server).get('/api/images?filename=sahara-desert&width=300')
+            expect(response.status).toBe(400)
+        })
+        test('a missing query parameter should return a message for the user', async (): Promise<void> => {
+            const response = await request(server).get('/api/images?filename=sahara-desert&width=300')
+            expect(response.text).toBe(
+                'query string missing - please use the following url format: /api/images?filename:YOURFILE&width:YOURWIDTH&height:YOURHEIGHT'
+            )
+        })
+        test('querying a non-existing filename should return status code 400', async (): Promise<void> => {
+            const response = await request(server).get('/api/images?filename=non-existent&width=300&height=300')
+            expect(response.status).toBe(400)
+        })
+        test('querying a non-existing filename should return a message for the user', async (): Promise<void> => {
+            const response = await request(server).get('/api/images?filename=non-existent&width=300&height=300')
+            expect(response.text).toBe('image not found')
+        })
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import app from './app'
 const PORT = 3000
 
 // eslint-disable-next-line no-console
-app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
+const server = app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
+
+export default server
